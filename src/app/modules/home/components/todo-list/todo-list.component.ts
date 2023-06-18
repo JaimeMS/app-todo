ad91cbc71fbeb58ce8a66bd88cc3d012d1a5c5e3
JaimeDevS <jaime_mendes91@yahoo.com.br>
2023-06-18 20:02:44 -0300

    adicionado o localStorage para armazenar os itens

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -8,12 +8,12 @@ import { TaskList } from '../../model/task-list';
 })
 export class TodoListComponent implements DoCheck {
   
-  public taskList:Array<TaskList> = [];
+  public taskList:Array<TaskList> = JSON.parse(localStorage.getItem("list") || '[]');
 
   constructor() {}
 
   ngDoCheck(): void {
-    this.taskList.sort((first, last) => Number(first.checked) - Number(last.checked));
+    this.setLocalStorage();    
   }
 
   public setEmitTaskList(event: string){
@@ -38,6 +38,13 @@ export class TodoListComponent implements DoCheck {
       if(confirm){
         this.deleteItemTaskList(index);
       }
+    }
+  }
+
+  public setLocalStorage() {
+    if(this.taskList){
+      this.taskList.sort((first, last) => Number(first.checked) - Number(last.checked));
+      localStorage.setItem("list", JSON.stringify(this.taskList));
     }
   }
 }
