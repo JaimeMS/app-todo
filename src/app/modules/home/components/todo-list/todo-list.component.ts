9b2bbebe7ac325a8fcf85ff98b0dd40bf26d2e3c
JaimeDevS <jaime_mendes91@yahoo.com.br>
2023-06-18 19:50:48 -0300

    adicionada validação na lista

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck } from '@angular/core';
 import { TaskList } from '../../model/task-list';
 
 @Component({
@@ -6,13 +6,14 @@ import { TaskList } from '../../model/task-list';
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent implements OnInit {
+export class TodoListComponent implements DoCheck {
   
   public taskList:Array<TaskList> = [];
 
   constructor() {}
 
-  ngOnInit(): void {    
+  ngDoCheck(): void {
+    this.taskList.sort((first, last) => Number(first.checked) - Number(last.checked));
   }
 
   public setEmitTaskList(event: string){
@@ -31,4 +32,12 @@ export class TodoListComponent implements OnInit {
     }
   }
 
+  public validationInput(event: string, index: number){
+    if(!event.length){
+      const confirm = window.confirm("Task está vazia, deseja deletar?");
+      if(confirm){
+        this.deleteItemTaskList(index);
+      }
+    }
+  }
 }
