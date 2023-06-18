ca92e3632c5ad745cd68886c7b6a2a59c37167e8
JaimeDevS <jaime_mendes91@yahoo.com.br>
2023-06-18 18:50:51 -0300

    implementado o plano de fundo da lista vazia

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TaskList } from '../../model/task-list';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent {
+export class TodoListComponent implements OnInit {
+  
+  public taskList:Array<TaskList> = [
+   { task: "Minha Nova Task", checked: true },
+   { task: "Minha Nova Task 2", checked: false }
+  ];
 
+  constructor() {}
+
+  ngOnInit(): void {
+    
+  }
 }
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -9,6 +9,7 @@ import { TodoListComponent } from './components/todo-list/todo-list.component';
 
 //Page
 import { HomeComponent } from './pages/home/home.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -20,7 +21,8 @@ import { HomeComponent } from './pages/home/home.component';
     HomeComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ]
 })
 export class HomeModule { }
